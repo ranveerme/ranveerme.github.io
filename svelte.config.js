392f6b7de5d035ea4670b36635cc0eb92f227d27
Ranveer Singh Wilkhu <singh@ranveer.me>
2024-03-17 19:55:09 +0000

    Added dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -5,12 +5,36 @@ import {
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-    preprocess: vitePreprocess(),
+    preprocess: [
+        vitePreprocess(),
+        mdsvex({
+			extensions: extensions,
+			rehypePlugins: [
+				rehypeExternalLinks, // Adds 'target' and 'rel' to external links
+				rehypeSlug, // Adds 'id' attributes to Headings (h1,h2,etc)
+				[
+					rehypeAutolinkHeadings,
+					{
+						// Adds hyperlinks to the headings, requires rehypeSlug
+						behavior: 'prepend',
+						properties: { className: ['heading-link'], title: 'Permalink', ariaHidden: 'true' },
+						content: {
+							type: 'element',
+							tagName: 'span',
+							properties: {},
+							children: [{ type: 'text', value: '#' }]
+						}
+					}
+				]
+			]
+		})
+    ],
 
     kit: {
-        adapter: adapter()
+        adapter: adapter(),
         paths: {
             base: process.env.NODE_ENV === 'production' ? '/ranveerme.github.io' : '',
+        }
     }
 };
 
